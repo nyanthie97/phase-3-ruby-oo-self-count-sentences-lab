1b14d51d151e90763f95b98fc21e1bcf6d8dff33
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-08-21 02:44:59 +0300

    commit tree 24b4aa9a13c0ac7f066d36473b013ceb775e8774

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    end_with?('.')
   end
 
   def question?
 
+    end_with?('?')
   end
 
   def exclamation?
 
+    end_with?('!')
   end
 
   def count_sentences
 
+    sentences = 0
+    punctuation = %w[. ? !]
+    self
+      .chars
+      .each
+      .with_index do |char, index|
+        if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+          sentences += 1
+        end
+      end
+    sentences
   end
-end
+end 
